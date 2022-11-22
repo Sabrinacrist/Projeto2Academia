d6fe013eba8cd4110676b7fe08cb74d051891db2
Sabrina <sc28750@gmail.com>
2022-11-22 20:35:55 -0300

    projeto

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyByOYhjDyWawNiYQeq4dELTFrQIpxRif-E",
+    authDomain: "projeto-academia-1168e.firebaseapp.com",
+    projectId: "projeto-academia-1168e",
+    storageBucket: "projeto-academia-1168e.appspot.com",
+    messagingSenderId: "904068979320",
+    appId: "1:904068979320:web:ddd8c9c45818ac82f5ab8d",
+    measurementId: "G-5LLPZZLNCK"
+  };
+  
+  firebase.initializeApp(firebaseConfig)
+  var database = firebase.database()
